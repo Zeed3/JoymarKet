59690869b0dd6daae690e0a1413df45d876eadec
zahwan <zahwan.satria@gmail.com>
2025-12-05 16:45:04 +0700

    Initial project setup

---
+++ src/main/Main.java
@@ -0,0 +1,23 @@
+package main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module JoymarKet {
+	opens main;
+	requires javafx.graphics;
+}
