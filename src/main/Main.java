65fb2c978915682e7a6a4ce5c994460335ce6922
zahwan <zahwan.satria@gmail.com>
2025-12-17 19:46:18 +0700

    commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,7 +12,6 @@ public class Main extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
 		StageManager.setStage(primaryStage);
 		StageManager.switchScene(null);
 	}
